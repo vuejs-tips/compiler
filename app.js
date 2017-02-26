b843d2dfa5401614f63c697bddd250299581728d
Marcos Neves <marcos.neves@gmail.com>
2017-02-26 15:28:55 -0300

    Share url

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 function compile (code) {
   code = code.replace(/\s*\n+\s*/g, ' ') // collapse whitespaces
+  code = code.replace(/>\s+/g, '>').replace(/\s+</g, '<') // collapse whitespaces between close/open tags
   code = Vue.compile(code) // https://vuejs.org/v2/api/#Vue-compile
   // https://vuejs.org/v2/guide/render-function.html#Template-Compilation
   code = code.staticRenderFns.toString().replace(/^function anonymous/, 'function staticRenderFns')
@@ -60,18 +61,24 @@ Vue.component('code-mirror', {
     }
   }
 })
-const samples = {
-    'v-model component': '<name-input v-model="fullName"></name-input>',
-    'v-model input': '<input v-model="fullName">',
-    'v-model textarea': `<!-- same as input -->\n<textarea v-model="fullName"></textarea>`,
-    'v-model checkbox': '<input v-model="fullName" type="checkbox">',
-    'v-model radio': '<input v-model="fullName" type="radio">',
-    'v-model select': '<select v-model="fullName"></select>',
-    'v-bind': '<input v-bind:placeholder="inputMessage">',
+var samples = {
+    'custom': '<!-- type your html here -->',
+    'v-model': '<name-input v-model="fullName"></name-input>',
+    'v-model_input': '<input v-model="fullName">',
+    'v-model_textarea': `<!-- same as input -->\n<textarea v-model="fullName"></textarea>`,
+    'v-model_checkbox': '<input v-model="fullName" type="checkbox">',
+    'v-model_radio': '<input v-model="fullName" type="radio">',
+    'v-model_select': '<select v-model="fullName"></select>',
     'v-on': '<input v-on:input="update">',
-    'v-on($event)': '<input v-on:input="update($event)">',
-    'v-on filter': `<!-- filters does not work with v-on -->\n<input v-on:input="update | debounce(500)">`,
-    'v-bind with filter': '<a v-bind:href="url | slugify">login</a>',
+    'v-on-event': '<input v-on:input="update($event)">',
+    'v-on_filter': `<!-- filters does not work with v-on -->\n<input v-on:input="update | debounce(500)">`,
+    'v-bind_filter': '<textarea v-bind:value="message | truncate(140)"></textarea>',
+    'v-bind': '<input v-bind:placeholder="inputMessage">',
+    'v-pre': `<textarea v-pre>{{noCompilation}}</textarea>`,
+    'v-cloak': `<div v-cloak></div>`,
+    'v-once': `<div v-once></div>`,
+    'v-text': `<span v-text="msg"></span>`,
+    'v-html': `<div v-html="html"></div>`,
     'v-show': '<a v-show="signedIn">logout</a>',
     'v-if': '<a v-if="signedIn">logout</a>',
     'v-else': `<nav>
@@ -79,28 +86,71 @@ const samples = {
   <a v-else>login</a>
 </nav>
 `,
-    'v-for': `<ul><!-- v-for can't be the root element -->
+
+  'v-else-if': `<div v-if="type === 'A'">
+  A
+</div>
+<div v-else-if="type === 'B'">
+  B
+</div>
+<div v-else-if="type === 'C'">
+  C
+</div>
+<div v-else>
+  Not A/B/C
+</div>`,
+
+'v-for': `<ul><!-- v-for can't be the root element -->
   <li v-for="todo, index in todos">
     {{todo.label}}
   </li>
 </ul>
-`
+`,
+
+  'slot': `<div>
+  <slot>
+    content
+  </slot>
+  <slot name="footer">
+    copyright
+  </slot>
+</div>`,
+
+  'slot-scoped': `<div class="parent">
+  <child>
+    <template scope="props">
+      <span>hello from parent</span>
+      <span>{{ props.text }}</span>
+    </template>
+  </child>
+</div>`,
+
+  'inline-template': `<my-component inline-template>
+  <div>
+    content
+  </div>
+</my-component>`,
+
+  'component-is': `<component is="OtherComponent"></component>`
 }
 
 // Throw error message as Exception, so I can catch Vue compilation errors
 console.error = function (msg) {throw msg}
 
+var urlParam = decodeURI(location.hash.substr(1))
+var input = urlParam ? samples[urlParam] || atob(urlParam) : localStorage.getItem('input') || '<div></div>'
+
 new Vue({
   el: '#app',
 
   data: {
-    input: localStorage.getItem('input') || '<div></div>', // restore after refresh
+    input: input, // restore after refresh
     error: '',
     code: '',
     version: Vue.version,
     samples,
-    showMenu: !!decodeURI(location.hash.substr(1)),
-    selectedMenu: decodeURI(location.hash.substr(1))
+    showMenu: !!samples[urlParam],
+    selectedMenu: urlParam
   },
 
   computed: {
@@ -114,6 +164,14 @@ new Vue({
       }
 
       return this.code
+    },
+
+    url () {
+      return location.protocol + '//' + location.host + location.pathname
+    },
+
+    shareUrl () {
+      return this.url + '#' + btoa(this.input)
     }
   },
 
