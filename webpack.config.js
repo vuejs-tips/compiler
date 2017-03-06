e0cd504324fd66e40fe22138c5524f81ba9c6ff2
Marcos Neves <marcos.neves@gmail.com>
2017-03-06 01:49:53 -0300

    Inline css and js into html

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const webpack = require.main.require('webpack')
+const HtmlWebpackPlugin = require.main.require('html-webpack-plugin')
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin')
 
 module.exports = {
   devtool: false,
@@ -8,6 +10,11 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       'process.env.VERSION': JSON.stringify(require('./package.json').version)
-    })
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      inlineSource: '.(js|css)$' // embed all javascript and css inline
+    }),
+    new HtmlWebpackInlineSourcePlugin()
   ]
 }
